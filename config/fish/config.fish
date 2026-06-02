85c6fcb2e4446db33d77bccedcaddb4952e8b7e5
Dmitrii Bozhko <dvbozhko@gmail.com>
2026-06-02 15:39:05 +0200

    added prefix+C tmux new-session binding, --git in ls alias, lsa as sesh startup, dir_length=1

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -83,7 +83,7 @@ end
 
 # File system aliases (eza)
 if _have eza
-    alias ls 'eza -lh --group-directories-first --icons=auto'
+    alias ls 'eza -lh --git --group-directories-first --icons=auto'
     alias lsa 'ls -a'
     alias lt 'eza --tree --level=2 --long --icons --git'
     alias lta 'lt -a'
